a1bfe25a9306c0bfcbaef58e9dde2496515b063c
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-23 02:39:25 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog 
+  
+  def name=(dog_name) #defined the instance method #setter
+    this_dogs_name = dog_name
+  end
+  
+  def name #the getter
+    this_dogs_name = "Lassie"  #this is the second method is responsible for reporting, or reading the name
+  end  
+end
+  
+ 
+  
